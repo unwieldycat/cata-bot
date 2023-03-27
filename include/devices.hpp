90c73a2cb9a65841fcaa1a92db316a961a9a0bf3
Thurston Yates <thurstonalexyates@gmail.com>
2023-03-27 07:47:10 -0400

    Initial commit

---
+++ include/devices.hpp
@@ -0,0 +1,6 @@
+#pragma once
+#include "main.h"
+#include "pros/motors.hpp"
+
+extern pros::Motor drive_l;
+extern pros::Motor drive_r;
---
+++ src/devices.cpp
@@ -0,0 +1,4 @@
+#include "devices.hpp"
+
+pros::Motor drive_l(1);
+pros::Motor drive_r(2);
