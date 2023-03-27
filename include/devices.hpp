eab4358b68e7961248ef17c3529a3977e54ab2fb
Thurston Yates <thurstonalexyates@gmail.com>
2023-03-27 11:53:38 -0400

    Add devices

--- include/devices.hpp
+++ include/devices.hpp
@@ -2,5 +2,19 @@
 #include "main.h"
 #include "pros/motors.hpp"
 
+// ========================== Device Declarations ========================== //
+
+// HID
+extern pros::Controller master_controller;
+
+// Drivetrain motors
 extern pros::Motor drive_l;
 extern pros::Motor drive_r;
+
+// Catapult motors
+extern pros::Motor primer;
+extern pros::Motor latch;
+
+// Claw motors
+extern pros::Motor claw_pinch;
+extern pros::Motor claw_vert;
--- src/devices.cpp
+++ src/devices.cpp
@@ -1,4 +1,21 @@
 #include "devices.hpp"
+#include "pros/misc.h"
 
+// =========================== Device Definitions =========================== //
+
+// HID
+pros::Controller master_controller(pros::E_CONTROLLER_MASTER);
+
+// Drivetrain motors
 pros::Motor drive_l(1);
 pros::Motor drive_r(2);
+
+// Catapult motors
+pros::Motor primer(3);
+pros::Motor latch(4);
+
+// Claw motors
+pros::Motor claw_pinch(5);
+pros::Motor claw_vert(6);
+
+// ============================ Device Functions ============================ //
