9b3eba795f02c51dd9c4bed7eb6f6ab40506c2a8
Thurston Yates <thurstonalexyates@gmail.com>
2023-04-03 11:03:34 -0400

    Calibrate potentiometer

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
  * All other competition modes are blocked by initialize; it is recommended
  * to keep execution time for this mode under a few seconds.
  */
-void initialize() {}
+void initialize() { latch_pot.calibrate(); }
 
 /**
  * Runs while the robot is in the disabled state of Field Management System or
